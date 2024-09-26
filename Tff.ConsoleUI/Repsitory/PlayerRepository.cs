487dc301c48daccac50902790cc5f3ee37931686
Pendik <Pendik@BIR>
2024-09-26 13:03:56 +0300

    Add project files.

---
+++ Tff.ConsoleUI/Repsitory/PlayerRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tff.ConsoleUI.Models;
+
+namespace Tff.ConsoleUI.Repsitory
+{
+    public class PlayerRepository : IRepository<Player, Guid>
+    {
+        public Player Add(Player entity)
+        {
+            BaseRepository.Players.Add(entity);
+            return entity;
+        }
+
+        public Player? Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Player> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Player? GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Player? Update(Guid id, Player entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tff.ConsoleUI/Repsitory/TeamRepository.cs
@@ -0,0 +1,32 @@
+﻿using Tff.ConsoleUI.Models;
+
+namespace Tff.ConsoleUI.Repsitory;
+
+// ClassAdı.MetodAdı();
+public class TeamRepository : IRepository<Team, int>
+{
+    public Team Add(Team entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Team? Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Team> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Team? GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Team? Update(int id, Team entity)
+    {
+        throw new NotImplementedException();
+    }
+}
